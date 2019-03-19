d0e4a8db74a30f6532365240a9c7e779c8a1d528
lucaswodtke <lucas.wodtke@hotmail.com>
2019-03-19 20:09:06 -0300

    Teste para o SMart GIt

--- Trabalho_I/Main.cpp
+++ Trabalho_I/Main.cpp
@@ -15,7 +15,7 @@ using namespace std;
 
 static void print_hello (GtkWidget *widget, gpointer   data)
 {
-  g_print ("Hello World\n");
+  g_print ("Hello World\n");//teste Smart Git
 }
 
 int main(int argc, char *argv[])

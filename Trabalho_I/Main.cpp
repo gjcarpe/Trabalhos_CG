1bc9e009069cb3c787b34b6991d41ed7f1894e99
G. J. Carpeggiani <gustavocarpeggiani@hotmail.com>
2019-03-21 04:04:45 -0300

    Atualização com exemplo

--- Trabalho_I/Main.cpp
+++ Trabalho_I/Main.cpp
@@ -1,6 +1,6 @@
 #include <string>
 #include <iostream>
-
+#include <gtk/gtk.h>
 #include "ListaEnc.hpp"
 #include "Testes.hpp"
 
@@ -12,12 +12,37 @@ using namespace std;
 // C++ Reference:
 // https://www.tutorialspoint.com/cplusplus/cpp_quick_guide.htm
 
-int main (int argc, char *argv[])
+static void print_hello (GtkWidget *widget, gpointer data)
 {
-  //Testes testes;
-  //testes.executar();
+  g_print ("Hello World\n");
+}
 
-  
+int main(int argc, char *argv[])
+{
+  GtkBuilder *builder;
+  GObject *window;
+  GObject *button;
+
+  gtk_init (&argc, &argv);
+
+  /* Construct a GtkBuilder instance and load our UI description */
+  builder = gtk_builder_new ();
+  gtk_builder_add_from_file (builder, "JanelaGlade.glade", NULL);
+
+  /* Connect signal handlers to the constructed widgets. */
+  window = gtk_builder_get_object (builder, "window");
+  g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
+
+  button = gtk_builder_get_object (builder, "button1");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
+
+  button = gtk_builder_get_object (builder, "button2");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
+
+  button = gtk_builder_get_object (builder, "quit");
+  g_signal_connect (button, "clicked", G_CALLBACK (gtk_main_quit), NULL);
+
+  gtk_main ();
 
   return 0;
 }

4eb00e55e0dc912499dd9cb618c6c42527ddc684
G. J. Carpeggiani <gustavocarpeggiani@hotmail.com>
2019-03-20 23:33:45 -0300

    Test Main

--- Trabalho_I/Main.cpp
+++ Trabalho_I/Main.cpp
@@ -1,4 +1,3 @@
-#include <gtk/gtk.h>
 #include <string>
 #include <iostream>
 
@@ -13,37 +12,9 @@ using namespace std;
 // C++ Reference:
 // https://www.tutorialspoint.com/cplusplus/cpp_quick_guide.htm
 
-static void print_hello (GtkWidget *widget, gpointer   data)
+int main (int argc, char *argv[])
 {
-  g_print ("Hello World\n");//teste Smart Git 2
-}
-
-int main(int argc, char *argv[])
-{
-  GtkBuilder *builder;
-  GObject *window;
-  GObject *button;
-
-  gtk_init (&argc, &argv);
-
-  /* Construct a GtkBuilder instance and load our UI description */
-  builder = gtk_builder_new ();
-  gtk_builder_add_from_file (builder, "builder.ui", NULL);
-
-  /* Connect signal handlers to the constructed widgets. */
-  window = gtk_builder_get_object (builder, "window");
-  g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
-
-  button = gtk_builder_get_object (builder, "button1");
-  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
-
-  button = gtk_builder_get_object (builder, "button2");
-  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
-
-  button = gtk_builder_get_object (builder, "quit");
-  g_signal_connect (button, "clicked", G_CALLBACK (gtk_main_quit), NULL);
-
-  gtk_main ();
-
+  Testes testes;
+  testes.executar();
   return 0;
 }

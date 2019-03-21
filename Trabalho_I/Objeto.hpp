acf8cb4bcf067461a9d635f07b5b2aea5dab2b31
G. J. Carpeggiani <gustavocarpeggiani@hotmail.com>
2019-03-20 23:20:31 -0300

    Revert "Relembra da inicialização da lista encadeada para o construtor padrao,e revisar algumas coisa de c++ que talvez possam estar erradas"
    This reverts commit 5f1d096de924394ee8e7cbbae6fe827101456df1.

--- Trabalho_I/Objeto.hpp
+++ Trabalho_I/Objeto.hpp
@@ -4,26 +4,18 @@
 #include <string>
 #include <iostream>
 #include "ListaEnc.hpp"
-#include "Coordenada.hpp"
+#include "Ponto.hpp"
 
 using namespace std;
 
+// Classe que representa um objeto: um conjunto de pontos interligados no espaço vetorial
 
-class Objeto // UM OBjeto pussui um nome e um lista encadeada de coordenadas
+class Objeto
 {
     public:
-        QString nome;
-        ListaEnc<Coordenada> pontos;
-
-        QString toString();
-
-        QString ObjetoGrafico::toString(){
-        QString txt = "";
-        for(int i=0;i<pontos.size();i++){
-            txt += "( " + QString::number(pontos[i].getX()) + " , " + QString::number(pontos[i].getY()) + " )";
-        }
-        return txt;
-    }
+        string nome;
+        string tipo;
+        ListaEnc<Ponto> coordenadas;
 };
 
 #endif
--- Trabalho_I/Ponto.hpp
+++ Trabalho_I/Ponto.hpp
@@ -1,27 +1,17 @@
 #ifndef PONTO_HPP
 #define PONTO_HPP
-#include "Coordenada.hpp"
-#include "Objeto.hpp"
 
+using namespace std;
 
-class Ponto : public Objeto{
+// Classe que representa um ponto no espaço vetorial
 
-public:
-    Coordenada coord1;
-
-    Ponto(QString nome, Coordenada coord);
-    Ponto(Coordenada coord);
-
-
-    Ponto::Ponto(QString nome, Coordenada coord){
-        this->nome = nome;
-        this->coord1 = coord;
-    }
-
-    Ponto::Ponto(Coordenada coord){
-        this->nome = "ponto_default";
-        this->coord1 = coord;
-    }
+class Ponto
+{
+    public:
+        int x;
+        int y;
+    //  int z; para implementação 3D posterior
+    /// int zero; ponto nulo constante da 4a coordenada para facilitar os cálculos
 };
 
-#endif // PONTO_HPP
+#endif

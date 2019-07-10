ce75672b05d3e977ad2f1140d73cf4b27ff39560
G. J. Carpeggiani <gustavocarpeggiani@hotmail.com>
2019-07-10 20:11:21 -0300

    Unity

---
+++ Animacao_CG/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private int count;
+    public Text countText;
+    public Text winText;
+
+    public Rigidbody rb;
+    public float speed;
+   
+    void Start()
+    {
+        this.count = 0;
+        rb = GetComponent<Rigidbody>();
+        this.setCountText();
+        this.winText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHor = Input.GetAxis("Horizontal");
+        float moveVer = Input.GetAxis("Vertical");
+        float inputJump = Input.GetAxis("Jump");
+        Vector3 movement = new Vector3(moveHor, 1.5f * inputJump, moveVer);
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            this.setCountText();
+            if(count == 8)
+            {
+                this.winText.text = "You win!";
+            }
+        }
+    }
+
+    void setCountText()
+    {
+        countText.text = "INE5420 - Computação Gráfica - 2019 / 1 - Trabalho III " +
+                         "- Animação com GameEngine\n Pontuação: " + count;
+    }
+}
